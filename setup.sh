c8edbb486e8567267479063127a7c41627e2d5e7
Jules Olleon <jolleon@gmail.com>
2017-03-24 20:01:33 +0000

    better setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-ln -sv ~/.dotfiles/.bashrc ~
-ln -sv ~/.dotfiles/.vimrc ~
-ln -sv ~/.dotfiles/.tmux.conf ~
+for FILE in .bashrc .vimrc .tmux.conf
+do
+  if [ -f ~/$FILE ]; then
+    mv ~/$FILE ~/$FILE.old
+    echo "Existing $FILE has been moved to $FILE.old"
+  fi
+  ln -sv ~/.dotfiles/$FILE ~
+done
 
-# install vim-plug
+echo Installing vim-plug...
 # https://github.com/junegunn/vim-plug/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo Installing vim plugins...
+sleep .5
+vim +PlugInstall +qall
